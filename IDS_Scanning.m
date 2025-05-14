a78452eea2bc0f540d1b69f7a4de4deea90b7bf2
ioanassenov <ioancho@umich.edu>
2025-05-14 15:57:14 -0400

    Implement second stage functionality

--- IDS_Scanning.m
+++ IDS_Scanning.m
@@ -25,7 +25,7 @@ DeviceManagerCLI.BuildDeviceList();
 
 % Will need to update serial number to correct device
 serial_num='97100466'; % Serial number for KIM101 controller in Prof. Oldham's lab
-timeout_val=60000;
+timeout=60000;
 
 %Connect to controller
 device = KCubeInertialMotor.CreateKCubeInertialMotor(serial_num);
@@ -44,22 +44,56 @@ try
     channelsEnums = channelsHandle.GetEnumValues();
     jogDirectionHandle = motCLI.AssemblyHandle.GetType('Thorlabs.MotionControl.KCube.InertialMotorCLI.InertialMotorJogDirection');
     jogDirectionEnums = jogDirectionHandle.GetEnumValues();
-    PDChannel1 = channelsEnums.GetValue(0); 
-    PDChannel2 = channelsEnums.GetValue(1);
+    
+    % Redefine .NET assembly properties in convenient variables
+    jogFwd = jogDirectionEnums.GetValue(0); % Jog Direction Forward
+    jogRev = jogDirectionEnums.GetValue(1); % Jog Direction Reverse
+    PD1 = channelsEnums.GetValue(0);        % Channel 1 is the x stage
+    PD2 = channelsEnums.GetValue(1);        % Channel 2 is the y stage
     
     % Zero the actuators
     disp("Zero actuators 1 & 2")
-    device.SetPositionAs(PDChannel1, 0);
-    device.SetPositionAs(PDChannel2, 0);
+    device.SetPositionAs(PD1, 0);
+    device.SetPositionAs(PD2, 0);
     
-    % Move X actuator to position
-    %device.MoveBy(PDchannel1, 1000, timeout_val);
-    device.Jog(PDChannel1, jogDirectionEnums.GetValue(0), timeout_val);
+    %% Define movement parameters
+    % Define new jog parameters object and configure it
+    jogParams = Thorlabs.MotionControl.KCube.InertialMotorCLI.JogParams;
+    jogParams.JogStepFwd = 2000;             % Set forward step size
+    jogParams.JogStepRev = 3300;             % Set backward step size (larger value due to reverse movement hysteresis)
+    jogParams.JogRate = 2000;                % Set jog speed (cycles/sec?)
+    device.SetJogParameters(PD1, jogParams); % Apply jog parameters to PD1
 
-catch error
+    jogParams.JogStepRev = 3000;
+    device.SetJogParameters(PD2, jogParams); % Apply jog parameters to PD2
+
+    %% Movements
+
+    for y = 1:1
+        for x = 1:1
+            fprintf("Loop count: "); disp(x);
+            disp("Starting forward jog...");
+            device.Jog(PD1, jogFwd, timeout);
+            disp("Finished forward jog.");
+            pause(2);
+            disp("Staring reverse jog...");
+            device.Jog(PD1, jogRev, timeout);
+            disp("Finished reverse jog.");
+        end
+        fprintf("Loop count: "); disp(y);
+            disp("Starting forward jog...");
+            device.Jog(PD2, jogFwd, timeout);
+            disp("Finished forward jog.");
+            pause(2);
+            disp("Staring reverse jog...");
+            device.Jog(PD2, jogRev, timeout);
+            disp("Finished reverse jog.");
+    end
+
+catch err
     disp("Error has caused the program to stop, disconnecting...")
-    disp(error.identifier);
-    disp(error.message);
+    disp(err.identifier);
+    disp(err.message);
 end
 
 %Disconnect from controller
