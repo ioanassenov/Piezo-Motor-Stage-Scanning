491893dd1699c8cb1777b79245fafc54299b8e76
ioanassenov <ioancho@umich.edu>
2025-05-13 22:24:55 -0400

    Initial commit

---
+++ KIM101_Scanning.m
@@ -0,0 +1,72 @@
+%% Header
+
+GitHub Repo: https://github.com/Thorlabs/Motion_Control_Examples/tree/main/Matlab
+
+% Title: KIM101.m
+% Created Date: 2024-01-16
+% Last modified date: 2024-07-02
+% Matlab Version: R2023b
+% Thorlabs DLL version: Kinesis 1.14.44
+%% Notes:
+%
+% Example for the KIM101
+
+%% Start of code
+clear all; close all; clc
+
+%% Add and Import Assemblies
+devCLI = NET.addAssembly('C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.DeviceManagerCLI.dll');
+genCLI = NET.addAssembly('C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.GenericMotorCLI.dll');
+motCLI = NET.addAssembly('C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.KCube.InertialMotorCLI.dll');
+
+import Thorlabs.MotionControl.DeviceManagerCLI.*
+import Thorlabs.MotionControl.GenericMotorCLI.*
+import Thorlabs.MotionControl.KCube.InertialMotorCLI.*
+
+%% Connect
+% Builds Device list
+DeviceManagerCLI.BuildDeviceList();
+
+% Will need to update serial number to correct device
+serial_num='97100466'; % Serial number for KIM101 controller in Prof. Oldham lab
+timeout_val=60000;
+
+%Connect to controller
+device = KCubeInertialMotor.CreateKCubeInertialMotor(serial_num);
+device.Connect(serial_num);
+
+% Try/Catch statement used to disconnect correctly if error occurs
+try
+    device.WaitForSettingsInitialized(5000);
+    
+    device.StartPolling(250);
+    device.EnableDevice();
+    pause(1) %wait to make sure device is enabled
+    
+    % Pull the Enums needed
+    channelsHandle = motCLI.AssemblyHandle.GetType('Thorlabs.MotionControl.KCube.InertialMotorCLI.InertialMotorStatus+MotorChannels');
+    channelsEnums = channelsHandle.GetEnumValues();
+    jogDirectionHandle = motCLI.AssemblyHandle.GetType('Thorlabs.MotionControl.KCube.InertialMotorCLI.InertialMotorJogDirection');
+    jogDirectionEnums = jogDirectionHandle.GetEnumValues();
+    
+    % Zero the actuator
+    fprintf("Zero the actuator\n")
+    device.SetPositionAs(channelsEnums.GetValue(0), 0);
+    
+    % Jog the Actuator
+    fprintf("Jog the actuator forwards\n")
+    device.Jog(channelsEnums.GetValue(0), jogDirectionEnums.GetValue(0), timeout_val);
+
+catch e
+    fprintf("Error has caused the program to stop, disconnecting..\n")
+    fprintf(e.identifier);
+    fprintf("\n");
+    fprintf(e.message);
+end
+
+%Disconnect from controller
+device.StopPolling();
+device.Disconnect();
+
+%% Close Simulations (Comment out for real device)
+%SimulationManager.Instance.UninitializeSimulations();
