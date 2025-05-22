82fd1482fd023ed85600be9772ba2a896d19c0fe
ioanassenov <ioancho@umich.edu>
2025-05-22 16:57:45 -0400

    Implement ability to scan across single line

---
+++ line_scan.m
@@ -0,0 +1,96 @@
+% Title: Inertial Drive Stage (IDS) Single Line Test Scan
+% Filename: line_scan.m
+% Author: Ioan Assenov
+%
+% Description: This file is used as a test to both move the stages and
+% record data from the photomultiplier tube at the same time.
+
+clear;
+
+%% DAQ Setup
+dq = daq("ni"); % Initialize a DataAcquisition interface object for an NI device
+dq.Rate = 2e6;  % Set rate (Hertz)
+dqID = "PCIE6374_BNC"; % DAQ ID number is based on the PCI card
+in1 = addinput(dq, dqID, "ai0", "Voltage"); % Create input channel that we read data from
+
+%% Stage Movement Setup
+devCLI = NET.addAssembly(fullfile(pwd, "kinesis_dlls\Thorlabs.MotionControl.DeviceManagerCLI.dll"));
+genCLI = NET.addAssembly(fullfile(pwd, "kinesis_dlls\Thorlabs.MotionControl.GenericMotorCLI.dll"));
+motCLI = NET.addAssembly(fullfile(pwd, "kinesis_dlls\Thorlabs.MotionControl.KCube.InertialMotorCLI.dll"));
+
+import Thorlabs.MotionControl.DeviceManagerCLI.*
+import Thorlabs.MotionControl.GenericMotorCLI.*
+import Thorlabs.MotionControl.KCube.InertialMotorCLI.*
+
+% Builds Device list
+DeviceManagerCLI.BuildDeviceList();
+
+% Will need to update serial number to correct device
+serial_num='97100466'; % Serial number for KIM101 controller in Prof. Oldham's lab
+timeout=60000;
+
+% Connect to controller
+device = KCubeInertialMotor.CreateKCubeInertialMotor(serial_num);
+device.Connect(serial_num);
+
+% Try/Catch statement used to disconnect stage controller correctly if error occurs
+try
+    device.WaitForSettingsInitialized(5000);
+    
+    device.StartPolling(250);
+    device.EnableDevice();
+    pause(1) % Wait to make sure device is enabled
+    
+    % Pull the Enums needed
+    channelsHandle = motCLI.AssemblyHandle.GetType('Thorlabs.MotionControl.KCube.InertialMotorCLI.InertialMotorStatus+MotorChannels');
+    channelsEnums = channelsHandle.GetEnumValues();
+    
+    % Redefine .NET assembly properties in convenient variables
+    PD1 = channelsEnums.GetValue(0);        % Channel 1 is the x stage
+    PD2 = channelsEnums.GetValue(1);        % Channel 2 is the y stage
+    
+    % Zero the actuators
+    disp("Zero actuators 1 & 2")
+    device.SetPositionToZero(PD1);
+    device.SetPositionToZero(PD2);
+    
+    %% Define movement parameters
+    % Define new drive parameters object and configure it
+    driveParams = Thorlabs.MotionControl.KCube.InertialMotorCLI.DriveParams;
+    driveParams.StepRate = 2000;
+    device.SetDriveParameters(PD1, driveParams); % Apply drive parameters to PD1
+    device.SetDriveParameters(PD2, driveParams); % Apply drive parameters to PD2
+
+    % Define convenient MoveBy() function handles
+    move1 = @(steps) device.MoveBy(PD1, int32(steps), timeout);
+    move2 = @(steps) device.MoveBy(PD2, int32(steps), timeout);
+
+    % ######################## Movements ########################
+    increment = 50; % [steps] define the distance y moves after each row is scanned
+    endXPos = 7000;  % [steps] define the final X value of the row
+    data = zeros(1, endXPos/increment); % Initialize empty data vector
+
+    % Move through entire X range (subtract increment since we start at 0)
+    for i=0:increment:(endXPos-increment)
+        % Read the data for 1ms, rename the variable for convenience, take
+        % the mean value, and store it for the given position.
+        rawData = read(dq, seconds(100e-6));
+        rawData = renamevars(rawData, "PCIE6374_BNC_ai0", "ai0");
+        data(i/increment + 1) = mean(rawData.ai0); % Store the averaged data for the current position
+        disp(data(i/increment + 1))
+        
+        % Move the x stage by the defined increment
+        move1(increment);
+    end
+    
+catch err
+    disp("Error has caused the program to stop, disconnecting...")
+    disp(err.identifier);
+    disp(err.message);
+    rethrow(err)
+end
+
+%% Disconnect from controller
+disp("Program ended, disconnecting from controller...")
+device.StopPolling();
+device.Disconnect();
