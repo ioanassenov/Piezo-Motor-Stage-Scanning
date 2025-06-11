afa1ed0256a4f42ffcedb9d84724c47bf9b1dfc2
ioanassenov <ioancho@umich.edu>
2025-06-11 18:13:35 -0400

    Add linear mems simulation script

---
+++ mems_simulation/README.md
@@ -0,0 +1,27 @@
+# Linear MEMS Simulation
+This is a linear time-invariant simulation of the mems mirror's scanning
+pattern. It solves the following equations using a state space
+representation:
+
+$J_x \ddot\theta_x + c_x \dot\theta_x + k_x \theta_x = g_x V_{drive}$
+
+$J_y \ddot\theta_y + c_y \dot\theta_y+ k_y \theta_y = g_x V_{drive}$
+
+where the drive voltage input is defined by
+
+$V_{drive} = V_{DC} + V_{AC, x}\sin(2 \pi f_xt) + V_{AC,y}\sin(2 \pi f_y t + \phi_d)$
+
+With $V_{DC}$, $V_{AC}$ (x and y), $f$(x and y), and $\phi_d$ being
+arbitrary constants. $\theta_x(t)$ and  represent the angular position of
+the mirror over time.
+
+## MEMS Mirror
+The mirror is an ellipse with a major axis of 600 micrometers and a minor
+axis of 400 micrometers. The stiffnesses are defined based on the natural
+frequencies:
+
+$\frac{1}{2\pi} \sqrt{\frac{k_x}{J_x}} = 600 \text{Hz}$ and
+$\frac{1}{2\pi} \sqrt{\frac{k_y}{J_y}} = 400 \text{Hz}$
+
+These values for the natural frequencies are based on experimental data
+(presumably).
---
+++ mems_simulation/mems_sim_linear.m
@@ -0,0 +1,87 @@
+clear; close all;
+
+% ---------------- Geometry & gain & constant definitions -----------------
+
+% Define control constants
+Vdc = 10; % DC voltage
+Vacx = 5; % AC voltage component in x direction
+Vacy = 5; % AC voltage component in y direction
+fx = 60;  % frequency in x direction (Hz)
+fy = 60;  % frequency in y direction (Hz)
+phiy = pi/4; % phase shift in y direction
+t = 0:0.001:2; % time vector from 0 to 1 second with 1 ms interval
+
+% Mirror geometry
+a = 600e-6/2; % Length of semi-major axis [m]
+b = 400e-6/2; % Length of semi-minor axis [m]
+h = 20e-6;    % Mirror thickness [m]
+V = pi*a*b*h; % Volume of mirror [m^3]
+
+% Define mirror properties
+rho = 2300;  % Density of silicon [kg/m^3]
+m = rho * V; % Mass [kg];
+% Mass moment of inertia about each axis [kg*m^2]
+Jx = (1/4) * m * a^2 + (1/12) * m * b^2; % Minor axis
+Jy = (1/4) * m * b^2 + (1/12) * m * a^2; % Major axis
+% Stiffness and damping properties
+zeta = 0.05; % damping ratio estimate
+kx = (600*2*pi)^2*Jx;  % spring constant in x direction
+ky = (1400*2*pi)^2*Jy; % spring constant in y direction
+cx = 2*zeta*sqrt(kx*m);% damping coefficient in x direction
+cy = 2*zeta*sqrt(ky*m);% damping coefficient in y direction
+gx = 1;                % Drive voltage (input) gain
+
+
+% ----------------------- Input voltage definition ------------------------
+
+% Drive voltage definition
+Vdrive = Vdc + Vacx .* sin(2*pi*fx*t) + Vacy .* sin(2*pi*fy*t + phiy);
+
+
+% ------------------------ State space definition -------------------------
+
+% let x = [thetax, thetay, thetax', thetay']
+A = [0, 0, 1, 0;
+     0, 0, 0, 1;
+     -kx/Jx, 0, -cx/Jx, 0;
+     0, -ky/Jy, 0, -cy/Jy];
+B = [0; 0; gx; gx];
+C = [1, 0, 0, 0;
+     0, 1, 0, 0];
+D = 0;
+
+sys = ss(A, B, C, D);
+
+
+% ---------------------- Model for different inputs -----------------------
+
+% Simulate the step response of the system
+[yOut, tOut] = step(sys, t);
+
+% Simulate the Vdrive response of system
+[yDrive, tDrive] = lsim(sys, Vdrive, t);
+
+
+% ----------------------- Plot for different inputs -----------------------
+
+% Plot step response
+figure;
+subplot(2, 1, 1);
+plot(tOut, yOut);
+title('Step Response');
+xlabel('Time [s]'); ylabel('Output Response [rad]');
+subplot(2, 1, 2);
+plot([0, t(end)], [1, 1]);
+title('Input voltage');
+xlabel('Time [s]'); ylabel('Driving Voltage [V]');
+
+% Plot driving voltage response
+figure;
+subplot(2, 1, 1);
+plot(tDrive, yDrive);
+title('Drive Voltage');
+xlabel('Time [s]'); ylabel('Output Response [rad]');
+subplot(2, 1, 2);
+plot(t, Vdrive);
+title('Input voltage');
+xlabel('Time [s]'); ylabel('Driving Voltage [V]');
