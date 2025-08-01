0d91a82a632d9d01648d86e2bb6b86f285456d71
ioanassenov <ioancho@umich.edu>
2025-08-01 10:04:32 -0400

    Update description in quick_scan.m

--- quick_scan.m
+++ quick_scan.m
@@ -5,7 +5,7 @@
 % Description: This file is used as a test to both move the stages and
 % record data from the photomultiplier tube at the same time. It scans
 % along a given width and height (in steps) while continuously collecting
-% data. It then parses it and displays the data in a heatmap visualization.
+% data.
 %
 % Program output: rawData.mat -> a cell array with the raw data from the
 % photomultiplier tube organized by row.
